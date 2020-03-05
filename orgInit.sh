74f4fb5396d1147e40bc9b15adcef88b2136b9c2
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-04 21:37:39 -0800

    Added command to install mobile connected apps

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o security.demo
+# Install https://appexchange.salesforce.com/listingDetail?listingId=a0N3000000B4cUuEAJ
+sfdx force:package:install -p 04t3A000001AJf2QAGsfd
 sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Admin' LastName='User'"
 sfdx force:source:push
 sfdx force:user:permset:assign -n MobileSecurity
