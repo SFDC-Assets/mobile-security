0bab17704730081da8b1a10ab116c05a535bfe98
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-04-03 13:22:08 -0700

    Moved password reset to after custom attributes

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,6 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 sfdx force:package:install -p 04t3A000001AJf2QAG --wait 20
 sfdx force:source:push
 sfdx force:user:permset:assign -n MobileSecurity
-sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx shane:connectedapp:attributes -n "Salesforce for iOS" -a customAttributes.json
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
